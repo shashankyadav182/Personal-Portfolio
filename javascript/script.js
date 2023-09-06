3cc3055f1efbd55bb86a56667c571da36336c003
shashankyadav182 <144205413+shashankyadav182@users.noreply.github.com>
2023-09-06 11:22:50 +0530

    updates

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+
+
+< src="three.r134.min.js">
+< src="vanta.clouds.min.js">
+VANTA.CLOUDS({
+  el: "#your-element-selector",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00
+})
---
+++ projects/leverexperts/js/script.js
@@ -0,0 +1,184 @@
+function reliableOver(x){
+    x.innerHTML=`<h2>Reliability</h2>
+    <p>We provide accurate ,reilable and ethical services with our expert staff. we apply the fastestasnd most reliable methods for your brand</p>`;
+    // x.innerHTML="<p>We provide accurate ,reilable and ethical services with our expert staff. we apply the fastestasnd most reliable methods for your brand</p>"
+}
+function reliableOut(y){
+    y.innerHTML=`<i class="fas fa-user-tie" style="font-size: 100px;"></i>
+    <h1 style="font-size: 50px;">Reliability</h1>`
+}
+function loyaltyOver(x){
+    x.innerHTML=`<h2>loyalty</h2>
+    <p>our long-term work continues until the job is finished.we establish solid and long-term
+        relationships with all the companies we work with</p>`
+}
+function loyaltyOut(y){
+    y.innerHTML=`<i class="fas fa-landmark" style="font-size: 100px;" ></i>
+    <h1 style=" font-size: 50px;">loyalty</h1>`
+
+}
+function experienceOver(x){
+    x.innerHTML=`<h2>Experience</h2>
+    <p>We provide accurate ,reilable and ethical services with our expert staff. we apply the fastest
+        asnd most reliable methods for your brand</p>`
+}
+function experienceOut(y){
+    y.innerHTML=`<i class="fas fa-landmark" style="font-size: 100px;" ></i>
+    <h1 style=" font-size: 50px;">Experience</h1>`
+    
+
+}
+function supportOver(x){
+    x.innerHTML=`<h2>support</h2>
+    <p>We provide accurate ,reilable and ethical services with our expert staff. we apply the fastest
+        asnd most reliable methods for your brand</p>`
+}
+function supportOut(y){
+    y.innerHTML=`<i class="far fa-handshake " style="font-size: 100px;"></i>
+    <h1 style="font-size: 50px;">support</h1>`
+}
+function financeover(x){
+    x.innerHTML=`<h5 class="card-title" style="font-size:30px;">Financial planning</h5>
+    <div>
+        <p class="card-text">We structure your short and long-term financial situation, savings,
+            income and investments in accordance with your goals and expectations</p>
+        <a href="#" class="btn btn-dark">Review<i class="fas fa-arrow-right"></i></a>
+    </div>`
+}
+function financeout(y){
+    y.innerHTML=`<i class="fas fa-tachometer-alt  p-5" style="font-size: 100px;"></i>
+    <h5 class="card-title">Financial planning</h5>
+    <div>
+        <p class="card-text">We structure your short and long-term financial situation, savings,
+            income and investments in accordance with your goals and expectations</p>
+    </div>`
+}
+function projectOver(x){
+    x.innerHTML=`<h5 class="card-title" style="font-size:30px;">Project Finance</h5>
+    <div>
+       
+        <p class="card-text">By strengthening your capital structure, we ensure that you use finance
+            in the most accurate and effective way, thus enabling you to earn high profits.</p>
+         <a href="#" class="btn btn-dark">Review<i class="fas fa-arrow-right"></i></a>
+    </div>`
+}
+function projectOut(y){
+    y.innerHTML=`<i class="fas fa-balance-scale-left p-5" style="font-size: 100px;"></i>
+    <h5 class="card-title">Project Finance</h5>
+    <div>
+       
+        <p class="card-text">By strengthening your capital structure, we ensure that you use finance
+            in the most accurate and effective way, thus enabling you to earn high profits.</p>
+        <!-- <a href="#" class="btn btn-dark">Review<i class="fas fa-arrow-right"></i></a> -->
+    </div>`
+}
+function riskOver(x){
+    x.innerHTML=`<h5 class="card-title" style="font-size:30px;">Risk management</h5>
+    <div>
+        
+        <p class="card-text">It is the identification of risks, the determination of their effects,
+            and the management and control of the risk reduction in process.</p>
+        <a href="#" class="btn btn-dark ">Review<i class="fas fa-arrow-right"></i></a>
+    </div>`
+}
+function riskOut(y){
+    y.innerHTML=`<i class="fas fa-landmark p-5" style="font-size: 100px;"></i>
+    <h5 class="card-title">Risk management</h5>
+    <div>
+        
+        <p class="card-text">It is the identification of risks, the determination of their effects,
+            and the management and control of the risk reduction in process.</p>
+        <a href="#" class="btn btn-dark ">Review<i class="fas fa-arrow-right"></i></a>
+    </div>`
+}
+function riskOver(x){
+    x.innerHTML=`<h5 class="card-title" style="font-size:30px;">Risk management</h5>
+    <div>
+        
+        <p class="card-text">It is the identification of risks, the determination of their effects,
+            and the management and control of the risk reduction in process.</p>
+        <a href="#" class="btn btn-dark ">Review<i class="fas fa-arrow-right"></i></a>
+    </div>`
+}
+function riskOut(y){
+    y.innerHTML=`<i class="fas fa-landmark p-5" style="font-size: 100px;"></i>
+    <h5 class="card-title">Risk management</h5>
+    <div>
+        
+        <p class="card-text">It is the identification of risks, the determination of their effects,
+            and the management and control of the risk reduction in process.</p>
+        <a href="#" class="btn btn-dark ">Review<i class="fas fa-arrow-right"></i></a>
+    </div>`
+}
+function riskOver(x){
+    x.innerHTML=`<h5 class="card-title" style="font-size:30px;">Risk management</h5>
+    <div>
+        
+        <p class="card-text">It is the identification of risks, the determination of their effects,
+            and the management and control of the risk reduction in process.</p>
+        <a href="#" class="btn btn-dark ">Review<i class="fas fa-arrow-right"></i></a>
+    </div>`
+}
+function riskOut(y){
+    y.innerHTML=`<i class="fas fa-landmark p-5" style="font-size: 100px;"></i>
+    <h5 class="card-title">Risk management</h5>
+    <div>
+        
+        <p class="card-text">It is the identification of risks, the determination of their effects,
+            and the management and control of the risk reduction in process.</p>
+        <a href="#" class="btn btn-dark ">Review<i class="fas fa-arrow-right"></i></a>
+    </div>`
+}
+function riskOver(x){
+    x.innerHTML=`<h5 class="card-title" style="font-size:30px;">Risk management</h5>
+    <div>
+        
+        <p class="card-text">It is the identification of risks, the determination of their effects,
+            and the management and control of the risk reduction in process.</p>
+        <a href="#" class="btn btn-dark ">Review<i class="fas fa-arrow-right"></i></a>
+    </div>`
+}
+function riskOut(y){
+    y.innerHTML=`<i class="fas fa-landmark p-5" style="font-size: 100px;"></i>
+    <h5 class="card-title">Risk management</h5>
+    <div>
+        
+        <p class="card-text">It is the identification of risks, the determination of their effects,
+            and the management and control of the risk reduction in process.</p>
+    </div>`
+}
+function texOver(x){
+    x.innerHTML=`<h5 class="card-title" style="font-size:30px;">Texation</h5>
+    <div>
+        
+        <p class="card-text">It is the payment of taxes in the most appropriate way by minimizing
+            the incomes arising from economic and commercial transactions.</p>
+        <a href="#" class="btn btn-dark">Review<i class="fas fa-arrow-right"></i></a>
+    </div>`
+}
+function texOut(y){
+    y.innerHTML=`<i class="fas fa-city p-5" style="font-size: 100px;"></i>
+    <h5 class="card-title">Texation</h5>
+    <div>
+        
+        <p class="card-text">It is the payment of taxes in the most appropriate way by minimizing
+            the incomes arising from economic and commercial transactions.</p>
+    </div>`
+}
+function stragicOver(x){
+    x.innerHTML=`<h5 class="card-title" style="font-size:30px;">stragic management</h5>
+    <div>
+    <p class="card-text">Strategic management culture and its transformation into management
+                                systems lie on the basis of corporate sustainability and competitiveness.</p>
+                            <a href="#" class="btn btn-dark">Review<i class="fas fa-arrow-right"></i></a>
+    </div>`
+}
+function stragicOut(y){
+    y.innerHTML=`<i class="fas fa-user-shield p-5" style="font-size: 100px;"></i>
+    <h5 class="card-title">stragic management</h5>
+    <div>
+        <p class="card-text">Strategic management culture and its transformation into management
+            systems lie on the basis of corporate sustainability and competitiveness.</p>
+        <!-- <a href="#" class="btn btn-dark">Review<i class="fas fa-arrow-right"></i></a> -->
+    </div>`
+}
